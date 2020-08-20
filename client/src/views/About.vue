852cf7f9350d8d5caa99ff8e6344697f70a4febb
izzul003 <izzulislam03@gmail.com>
2020-08-20 23:47:47 +0700

    mau lanjut tugas yang lain dulu ka hehe mudah2an udh done

---
+++ client/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>website untuk membelanjakan uang Arnold. User bebas membeli barang apa saja selama saldo nya masih mencukupi</p>
+  </div>
+</template>
+<style scoped>
+.about {
+  height: 100vh;
+}
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home container">
+    <b-jumbotron bg-variant="white" class="mb-1">
+      <img alt="Vue logo" src="../assets/bapakarnold.png">
+      <br><br>
+      <h1 class="font-weight-bold">Spend Mr. Arnold's Money</h1>
+    </b-jumbotron>
+    <b-navbar sticky="true" variant="warning" class="mt-0 justify-content-center">
+      <h3>Rp. {{ money.toLocaleString() }} left</h3>
+    </b-navbar>
+    <b-row class="mt-1 d-flex flex-col">
+      <ProductCard v-for="(product, i) in products" :key="i" :product="product" @increment="increment" @spendMoney="spendMoney" :money="money"></ProductCard>
+    </b-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import io from 'socket.io-client'
+import ProductCard from '../components/ProductCard'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      products: [],
+      money: 0
+    }
+  },
+  components: {
+    ProductCard
+  },
+  methods: {
+    increment (id) {
+      console.log(id)
+      this.socket.emit('new-message', { id: id })
+    },
+    spendMoney (id) {
+      this.socket.emit('new-money', { id: id })
+    }
+  },
+  mounted () {
+    this.socket = io.connect('https://mr-arold-spend-money.herokuapp.com/')
+
+    // event listener
+    this.socket.on('init', (payload) => {
+      this.products = payload
+    })
+
+    this.socket.on('new-message', (payload) => {
+      this.products = payload
+    })
+
+    this.socket.on('init-money', payload => {
+      this.money = payload
+    })
+
+    this.socket.on('new-money', payload => {
+      this.money = payload
+    })
+  }
+}
+</script>
+<style scoped>
+</style>
