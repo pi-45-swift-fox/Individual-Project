2ccf5c97e9b5b6362104760a1fd68cf072459ea4
MarnatalChristian <christian.kodz@gmail.com>
2020-08-21 17:34:50 +0700

    before deploy

---
+++ client/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p> Menghabiskan uang Arnold</p>
+  </div>
+</template>
+<style scoped>
+.about {
+  height: 100vh;
+}
+</style>
